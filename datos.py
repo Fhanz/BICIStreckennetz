996d6dfa21818ef27401ef342aafbe391d248d57
Fhanz <marco.a.gaymer@uc.cl>
2022-05-24 05:02:52 -0400

    ✨Finished modelo.py with real data✨

--- datos.py
+++ datos.py
@@ -1,22 +1,163 @@
 import pandas
+import timeit
+
+start = timeit.default_timer()
+
+
 loop = True
 ciclovias = []
 calles = []
 dias = 0
 
-p_generales = pandas.read_excel(io = "parametros.xlsx", sheet_name = "Parametros generales")
-p_ciclovias = pandas.read_excel(io = "parametros.xlsx", sheet_name = "Parametros de ciclovias")
-p_calles    = pandas.read_excel(io = "parametros.xlsx", sheet_name = "Parametros de calles")
-p_tiempo    = pandas.read_excel(io = "parametros.xlsx", sheet_name = "Parametros de tiempo")
-p_cic_calle = pandas.read_excel(io = "parametros.xlsx", sheet_name = "Parametros de ciclovias y calle")
+p_generales = pandas.read_excel(io="parametros.xlsx", sheet_name="Parametros generales")
+p_ciclovias = pandas.read_excel(
+    io="parametros.xlsx", sheet_name="Parametros de ciclovias"
+)
+p_calles = pandas.read_excel(io="parametros.xlsx", sheet_name="Parametros de calles")
+p_tiempo = pandas.read_excel(io="parametros.xlsx", sheet_name="Parametros de tiempo")
+p_cic_calle = pandas.read_excel(
+    io="parametros.xlsx", sheet_name="Parametros de ciclovias y calle"
+)
+
+p_generales = p_generales.fillna(0)
+p_ciclovias = p_ciclovias.fillna(0)
+p_calles = p_calles.fillna(0)
+p_tiempo = p_tiempo.fillna(0)
+p_cic_calle = p_cic_calle.fillna(0)
+
 
 # indices
+
 
 def c_():
     ciclovias = p_ciclovias["c"].max()
     return ciclovias
 
-print(c_())
+
+def n_():
+    calles = p_calles["n"].max()
+    return calles
+
+
+def t_():
+    dias = int(p_tiempo["t"].max())
+    return dias
+
+#print(c_())
+#print(n_())
+#print(t_())
+
+# parametros generales
+
+def p_():
+    presupuesto = p_generales["P"][1]
+    return presupuesto
+
+def s_():
+    sueldo = p_generales["S"][1]
+    return sueldo
+
+#print(f"P: {p_()}")
+#print(f"S: {s_()}")
+
+# parametros relativos a las ciclovias:
+def K_c():
+    lista = []
+    for c in range(c_()):
+        lista.append(p_ciclovias["Kc [$/m]"][c])
+    return lista
+
+def G_c():
+    lista = []
+    for c in range(c_()):
+        lista.append(p_ciclovias["Gc [$]"][c])
+    return lista
+
+def D_c():
+    lista = []
+    for c in range(c_()):
+        lista.append(p_ciclovias["Dc [$/m]"][c])
+    return lista
+
+def U_c():
+    lista = []
+    for c in range(c_()):
+        lista.append(p_ciclovias["Uc [dias/m]"][c])
+    return lista
+
+def J_c():
+    lista = []
+    for c in range(c_()):
+        lista.append(p_ciclovias["Jc [personas]"][c])
+    return lista
+
+
+#print(p_calles["In"][3])
+
+#print(f"K_c: {K_c()}")
+#print(f"G_c: {G_c()}")
+#print(f"D_c: {D_c()}")
+#print(f"U_c: {U_c()}")
+#print(f"J_c: {J_c()}")
+
+
+# parametros relativos a las calles:
+
+def E_n():
+    lista = []
+    for n in range(n_()):
+        lista.append(p_calles["En"][n])
+    return lista
+
+def I_n():
+    lista = []
+    for n in range(n_()):
+        lista.append(p_calles["In"][n])
+    return lista
+
+def L_n():
+    lista = []
+    for n in range(n_()):
+        lista.append(p_calles["Ln"][n])
+    return lista
+
+def A_n():
+    lista = []
+    for n in range(n_()):
+        lista.append(p_calles["An"][n])
+    return lista
+
+# print(f"E_n: {E_n()}")
+# print(f"I_n: {I_n()}")
+# print(f"L_n: {L_n()}")
+# print(f"A_n: {A_n()}")
+
+
+# parametros relativos al tiempo
+
+def O_t():
+    lista = []
+    for t in range(t_()):
+        lista.append(int(p_tiempo["Ot"][t]/10 + 7))
+    return lista
+
+#print(f"O_t: {O_t()}")
+
+# Parámetros relativos a las ciclovias y las calles
+
+def H_nc():
+    lista = []
+    for n in range(n_()):
+        listita = []
+        for c in range(c_()):
+            listita.append(int(p_cic_calle[f"{c + 1}"][n]))
+        lista.append(listita)
+    return lista
+
+#print(f"H_nc: {H_nc()}")
+
+stop = timeit.default_timer()
+print('Time: ', stop - start)  
 
 # while loop:
 #     nueva_ciclovia = input("\nIngrese tipo de ciclovía:\n")
@@ -37,14 +178,13 @@ print(c_())
 
 # dias = int(input("\nDuracion del proyecto en dias:\n"))
 
-# # parametros generales
 
 # P = int(input("\nPresupuesto de la municipalidad:\n"))
 # T = dias
 # S = int(input("\nSueldo de trabajador externo:\n"))
 
 
-# # parametros relativos a las ciclovias:
+# 
 
 # K_c = []
 # for i in range(len(ciclovias)):
@@ -72,7 +212,7 @@ print(c_())
 #     J_c.append(j_nuevo)
 
 
-# # parametros relativos a las calles:
+# 
 
 # L_n = []
 # for i in range(len(calles)):
@@ -110,8 +250,7 @@ print(c_())
 #     o_nuevo = int(input(f"\nPersonal máximo disponible en el dia {i + 1}:\n"))
 #     O_t.append(o_nuevo)
 
-# # Parámetros relativos a las ciclovias y las calles
-
+# 
 # loop = True
 
 # H_nc = []
@@ -140,4 +279,3 @@ print(c_())
 
 # for i in range(len(calles)):
 #     print(f"calle numero {i}")
-
--- modelo.py
+++ modelo.py
@@ -1,86 +1,89 @@
 # from calendar import c
 from gurobipy import Model, GRB, quicksum
 import random
-import pandas
 from datos import K_c, G_c, D_c, U_c, J_c, L_n, A_n, I_n, E_n, O_t, H_nc, p_, t_, s_, n_, c_
+import timeit
+
+start = timeit.default_timer()
+
 
 random.seed(10)
 
-t = 10
-n = 6
-c = 3
+t = t_()
+n = n_()
+c = c_()
 
 # rangos
-C_ = range(c)
-#C_ = range(c_())
+#C_ = range(c)
+C_ = range(c_())
 
-N_ = range(n)
-#N_ =range(n_())
+#N_ = range(n)
+N_ =range(n_())
 
-T_ = range(t)
-#T_= range(t_())
+#T_ = range(t)
+T_= range(t_())
 
 
 # Parametros
 
 # Presupuesto de la municipalidad
-P = 88300
-#P=p_()
+# P = 88300
+P=p_()
 
 # Plazo maximo para terminar la construccion de las obras
-T = t
-#T=t_()
+#T = t
+T=t_()
 
 # Sueldo fijo diario para trabajador externo
-S = 130000000
-#S = s_()
-m
+#S = 130000000
+S = s_()
+
 # Costo de produccion de un metro de la ciclovia tipo c
-K = [2000, 3000, 5000]  # Ciclovia tipo [0] cuesta 0.002$ por km
-#K = K_c()
+#K = [2000, 3000, 5000]  # Ciclovia tipo [0] cuesta 0.002$ por km
+K = K_c()
 
 # Costo de produccion de senalizacion de ciclovia tipo c
-G = [20, 30, 50]  # Senalizacion de ciclovia tipo [0] cuesta 20$
-#G = G_c()
+#G = [20, 30, 50]  # Senalizacion de ciclovia tipo [0] cuesta 20$
+G = G_c()
 
 # Costo de mantencion por metro de ciclovia tipo c
-D = [2000, 3000, 5000]  # Ciclovia tipo [0] cuesta 0.002$ por km
-#D = D_c()
+#D = [2000, 3000, 5000]  # Ciclovia tipo [0] cuesta 0.002$ por km
+D = D_c()
 
 # Tiempo de construccion de la ciclovia tipo c
-U = [1, 2, 3]
-#U = U_c()
+#U = [1, 2, 3]
+U = U_c()
 
 # Personal requerido para construir ciclovia tipo c
-J = [2, 3, 4]
-#J = J_c()
+#J = [2, 3, 4]
+J = J_c()
 
 # Largo de la calle n en metros
-L = [1, 2, 3, 4, 5, 3]
-#L = L_n()
+#L = [1, 2, 3, 4, 5, 3]
+L = L_n()
 
 # Cantidad de personas que utilizan la calle n
-A = [50, 80, 12, 100, 60, 1000]
-#A = A_n()
+#A = [50, 80, 12, 100, 60, 1000]
+A = A_n()
 
 # Indice de accidentes que sufren bicicletas en la calle n
-I = [0.8, 0.5, 0.9, 1.0, 0.6, 0.2]
-#I = I_n()
+#I = [0.8, 0.5, 0.9, 1.0, 0.6, 0.2]
+I = I_n()
 
 # Existencia de ciclovia en la calle n
-E = [0, 0, 0, 0, 0, 0]
-#E = E_n()
+#E = [0, 0, 0, 0, 0, 0]
+E = E_n()
  
 # Personal disponible para trabajar en el dia t
-O = []
-for i in range(t):
-    O.append(7)
-#O = O_t()
+# O = []
+# for i in range(t):
+#     O.append(7)
+O = O_t()
 
 
 # Si en la calle n se puede construir
-H = [[0, 1, 0], [1, 1, 1], [1, 1, 0], [0, 1, 1], [1, 1, 1], [0, 1, 1]]
-#H = H_nc()
+#H = [[0, 1, 0], [1, 1, 1], [1, 1, 0], [0, 1, 1], [1, 1, 1], [0, 1, 1]]
+H = H_nc()
 
 
 # definmos un sub indice de [i,j,k] que son los que se meveran dentro de los indices, estos estan en orden [C_,N_,T_].
@@ -123,7 +126,7 @@ m.addConstrs(
 
 # R4
 m.addConstrs(
-    (quicksum(w[n, c, t] for t in T_) == x[n, c] * U[c] * L[n] for c in C_ for n in N_),
+    (quicksum(w[n, c, t] for t in T_) == x[n, c] * round(U[c] * L[n]) for c in C_ for n in N_),
     name="Cumplir plazo",
 )
 
@@ -139,6 +142,8 @@ m.addConstrs(
     name="Activacion/desactivacion variable W",
 )
 
+m.addConstrs(z[t] <= O[t] for t in T_) # restrccion para tener personal equitativo - considerar
+
 
 # Funcion Objetivo
 m.setObjective(
@@ -147,28 +152,60 @@ m.setObjective(
 
 # Imprimir Valor Objetivo
 m.optimize()
+
+# print("\n"+"-"*9+" Restricciones Activas "+"-"*9)
+# for constr in m.getConstrs():
+#     if constr.getAttr("slack") == 0:
+#         print(f"La restriccion {constr} está activa")
+
+
 print(f"\nEl valor objetivo es: {m.Objval}\n")
 #################################
-
+calles = 0
 print("Ciclovias planeadas:\n")
 for n_i in range(n):
     for c_i in range(c):
         if x[n_i, c_i].x != 0:
             print(f"En la calle {n_i + 1} se construira una ciclovia tipo {c_i + 1}")
+            calles += 1
+print(f"Se construyen {calles} ciclovias en total.")
 
 print(f"Trabajores externos contratados en proyecto: {quicksum(z[t].x for t in T_ )}")
+dinero_restante = P
+print(f"Dinero restante: {dinero_restante}")
+dias_sin_trabajar = 0
 
+spam = input("Quieres mostrar el itnerario diario? (s/n):\n")
 
-for t_i in range(t):
-    trabajo_en_curso = quicksum(w[n, c, t_i] for n in N_ for c in C_)
-    if float(str(trabajo_en_curso)[-5] + "." + str(trabajo_en_curso)[-3]) > 0:
+if spam.lower() in "si":
+    for t_i in range(t):
+        #trabajo_en_curso = quicksum(w[n, c, t_i] for n in N_ for c in C_)
+        #if float(str(trabajo_en_curso)[-5] + "." + str(trabajo_en_curso)[-3]) > 0:
         print(f"\n\nDia {t_i + 1}:\n")
+        flojeando = True
+        dinero_gastado = 0
         if z[t_i].x != 0:
             print(f"--Se contrataron {z[t_i].x} trabajadores extra--")
+            dinero_restante -= z[t_i].x*S
+            dinero_gastado +=z[t_i].x*S
         for n_i in range(n):
             for c_i in range(c):
                 if w[n_i, c_i, t_i].x != 0:
                     print(f"Se esta construyendo una ciclovia en la calle {n_i + 1}")
+                    dinero_gastado += ((L[n_i] * (D[c_i] + K[c_i]) + 2 * G[c_i])/(L[n_i]*U[c_i]))
+                    dinero_restante -= ((L[n_i] * (D[c_i] + K[c_i]) + 2 * G[c_i])/(L[n_i]*U[c_i]))
+                    flojeando = False
+        if flojeando:
+            dias_sin_trabajar += 1
+        print(f"Presupuesto utilizado: {dinero_gastado}")
+        print(f"Dinero restante: {dinero_restante}")
+
+    print(f"Hubo {dias_sin_trabajar} dias en los que no se construyeron ciclovias")
+
+
+
+stop = timeit.default_timer()
+print('Time: ', stop - start)  
 
 # (O_t - W_nc1 * J_c) + Z_1 >= 0
 # (10 - 3 * 3) =1 -> 1+z_t =0 -> -1
